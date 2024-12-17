822de085425d1b29358541ffa721133f9fde3620
Pavel Potemkin <pp@blum.io>
2024-12-17 15:18:37 +0400

    updates

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: isProd ? '/blum-memepad-example/' : '/',
   plugins: [
     vue(), 
     nodePolyfills({
